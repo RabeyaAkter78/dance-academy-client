5c05dc82dfe880a8cfc7476c5987f51390dbdc72
RabeyaAkter78 <rabeyariya251@gmail.com>
2023-06-07 10:33:46 +0600

    first commit

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,39 @@
+import image1 from '../../../assets/images/dance1.png'
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img src="https://i.ibb.co/HVBSmbn/corina-1-RGB.jpg"className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img src="https://media.istockphoto.com/id/1404175194/photo/ballerina-dance-ballet-dancer-in-red-dress-jumping-split-woman-in-ballerina-shoes-dancing-in.jpg?s=612x612&w=0&k=20&c=ulKfQhNR6viAQhMj-uvrXG3BHZNu8-Be_-1OanihWL8=" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img src={image1}className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
